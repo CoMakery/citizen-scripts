1143046c3e480eed757130e511a308aa3a55d482
David Balatero <dbalatero@gmail.com>
2016-10-21 17:03:49 -0700

    Passing spec to test pushit

--- spec/failing_spec.rb
+++
@@ -1,5 +0,0 @@
-describe "broken test" do
-  it "should fail" do
-    expect(2).to eq(3)
-  end
-end
---
+++ spec/passing_spec.rb
@@ -0,0 +1,5 @@
+describe "passing test" do
+  it "should not fail" do
+    expect(3).to eq(3)
+  end
+end
