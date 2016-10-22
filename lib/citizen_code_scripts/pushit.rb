14b7c928ce66dd6db0c1a2a7d3e42bac27ddb630
David Balatero <dbalatero@gmail.com>
2016-10-21 17:12:28 -0700

    Use the Test command on pushit

--- lib/citizen_code_scripts/pushit.rb
+++ lib/citizen_code_scripts/pushit.rb
@@ -1,6 +1,6 @@
 class CitizenCodeScripts::Pushit < CitizenCodeScripts::Base
   def run
     system('git pull --rebase')
-    CitizenCodeScripts::Rspec.run && system('git push origin master')
+    CitizenCodeScripts::Test.run && system('git push origin master')
   end
 end
