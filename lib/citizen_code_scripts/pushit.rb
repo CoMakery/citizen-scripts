96f6377f9e198fb1925e75b699b0200b9c306177
David Balatero <dbalatero@gmail.com>
2016-10-25 16:59:59 -0700

    Add desc for pushit

--- lib/citizen_code_scripts/pushit.rb
+++ lib/citizen_code_scripts/pushit.rb
@@ -1,4 +1,8 @@
 class CitizenCodeScripts::Pushit < CitizenCodeScripts::Base
+  def self.description
+    "Pulls code, runs test, pushes your code"
+  end
+
   def run
     CitizenCodeScripts::Update.run
     CitizenCodeScripts::Test.run
