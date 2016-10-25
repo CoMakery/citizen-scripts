646e5ee40644601a04d2488413ec92b9d510b948
David Balatero <dbalatero@gmail.com>
2016-10-25 16:32:00 -0700

    Update today-i with help/desc

--- lib/citizen_code_scripts/todayi.rb
+++ lib/citizen_code_scripts/todayi.rb
@@ -1,7 +1,26 @@
 class CitizenCodeScripts::TodayI < CitizenCodeScripts::Base
+  def self.help
+    <<-EOF
+citizen today-i
+
+Prints out a list of commit message names that you worked on today.
+EOF
+  end
+
+  def self.description
+    "Prints a list of commit msgs from today"
+  end
+
   def run
     date_string = Time.now.to_s.split(' ')[0]
-    lines = `git log --date=local --oneline --after="#{date_string} 00:00" --before="#{date_string} 23:59"`
+
+    lines = %x{
+      git log \
+        --date=local \
+        --oneline \
+        --after="#{date_string} 00:00" \
+        --before="#{date_string} 23:59"
+    }
 
     lines.each_line do |line|
       puts line.split(" ", 2)[1]
