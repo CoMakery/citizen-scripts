8829efe615eb42e02a0e5dde676f420b4c75c826
David Balatero <dbalatero@gmail.com>
2016-10-25 16:59:33 -0700

    Factor out update script

--- lib/citizen_code_scripts/update.rb
+++ lib/citizen_code_scripts/update.rb
@@ -12,40 +12,52 @@ EOF
   end
 
   def run
-    step "Pulling from git" do
-      system! "git pull --rebase"
-    end
-
-    step "Installing dependencies" do
-      system! 'gem install bundler --conservative'
-      system('bundle check') || system!('bundle install')
-    end
-
-    step "Updating database" do
-      system! 'rake db:migrate'
-    end
-
-    step "Removing old logs and tempfiles" do
-      system! 'rake log:clear tmp:clear'
-    end
-
-    step "Attempting to restart Rails" do
-      output = `bin/rails restart`
-
-      if $?.exitstatus > 0
-        skip_rails_restart_msg
-      else
-        puts output
-      end
-    end
+    pull_git
+    install_dependencies
+    update_db
+    remove_old_logs
+    restart_rails
   end
 
   private
 
-  def skip_rails_restart_msg
-    puts colorize(
-      :light_red,
-      "skipping restart, not supported in this version of Rails (needs >= 5)"
-    )
+  def pull_git
+    step "Pulling from git" do
+      system! "git pull --rebase"
+    end
+  end
+
+  def install_dependencies
+    step "Installing dependencies" do
+      system! 'gem install bundler --conservative'
+      system('bundle check') || system!('bundle install')
+    end
+  end
+
+  def update_db
+    step "Updating database" do
+      system! 'rake db:migrate'
+    end
+  end
+
+  def remove_old_logs
+    step "Removing old logs and tempfiles" do
+      system! 'rake log:clear tmp:clear'
+    end
+  end
+
+  def restart_rails
+    step "Attempting to restart Rails" do
+      output = `bin/rails restart`
+
+      if $?.exitstatus > 0
+        puts colorize(
+          :light_red,
+          "skipping restart, not supported in this version of Rails (needs >= 5)"
+        )
+      else
+        puts output
+      end
+    end
   end
 end
