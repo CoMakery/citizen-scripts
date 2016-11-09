180b44455fd90e560242feaaccc1e5e80ab9272a
Glenn Jahnke <glenn@gmail.com>
2016-11-09 10:32:29 -0800

    use better levenstein code that doesn't run for infinity

--- lib/citizen_code_scripts/levenstein.rb
+++ lib/citizen_code_scripts/levenstein.rb
@@ -1,11 +1,26 @@
 class Levenstein
   def self.edit_distance(s, t)
-    return t.size if s.empty?
-    return s.size if t.empty?
-    return [(edit_distance s.chop, t) + 1,
-            (edit_distance s, t.chop) + 1,
-            (edit_distance s.chop, t.chop) + (s[-1, 1] == t[-1, 1] ? 0 : 1)
-    ].min
+    m = s.length
+    n = t.length
+    return m if n == 0
+    return n if m == 0
+    d = Array.new(m+1) { Array.new(n+1) }
+
+    (0..m).each { |i| d[i][0] = i }
+    (0..n).each { |j| d[0][j] = j }
+    (1..n).each do |j|
+      (1..m).each do |i|
+        d[i][j] = if s[i-1] == t[j-1] # adjust index into string
+          d[i-1][j-1] # no operation required
+        else
+          [d[i-1][j]+1, # deletion
+           d[i][j-1]+1, # insertion
+           d[i-1][j-1]+1, # substitution
+          ].min
+        end
+      end
+    end
+    d[m][n]
   end
 
   def self.closest_match(needle, haystack)
