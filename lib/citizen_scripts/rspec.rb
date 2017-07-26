c17af66a1ff25d6e2a94747f7c246a7bf5975f0e
Rob Hanlon <robhanlon@gmail.com>
2017-07-26 16:25:21 -0700

    use bundle exec instead of Gem.bin_path for rspec

--- lib/citizen_scripts/rspec.rb
+++ lib/citizen_scripts/rspec.rb
@@ -12,7 +12,7 @@ class CitizenScripts::Rspec < CitizenScripts::Base
     require 'bundler/setup'
 
     step "Running RSpec" do
-      command = [Gem.bin_path('rspec-core', 'rspec')] + argv
+      command = ['bundle', 'exec', 'rspec', *argv]
       system!(command.join(" "))
     end
   end
